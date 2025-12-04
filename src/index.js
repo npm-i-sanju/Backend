2fc3f29311ed01b2328f016a3043c9373f5201a7
Sanju Roy <sanjuroy806@gmail.com>
2025-12-05 01:17:48 +0530

    Connet to MongoDB Atlas

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// 
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB();
+
+
+
+
+// (async() => {
+//     try{
+//         await mongoose.connect(`${process.env.
+//             MONGO_URI}/${DB_NAME}`)
+//     }catch(error){
+//         console.error("Error", error);
+//         throw error;
+//     }
+// })()
