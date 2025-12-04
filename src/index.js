4a33031c7b4bd103410625abf43ee4aa72948979
Sanju Roy <sanjuroy806@gmail.com>
2025-12-05 01:18:36 +0530

    cors i

--- src/index.js
+++ src/index.js
@@ -6,8 +6,17 @@ dotenv.config({
     path: "./.env"
 });
 
-connectDB();
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+        
+    })
+})
+.catch((error) => {
+    console.error("Error", error);
+    throw error;
+});
 
 
 
