041499c56a701db8ea986e293d3b4e067addd1d5
Sanju Roy <sanjuroy806@gmail.com>
2025-12-05 22:15:53 +0530

    util code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = 'Internal Server Error',
+        errors = [],
+        statck = ""
+
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        if (statck) {
+            this.stack = statck;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+
+export { ApiError };
