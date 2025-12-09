90f6c7c08d00565e9bbbffc737e1ff1bd88e5dcd
Sanju Roy <sanjuroy806@gmail.com>
2025-12-10 01:31:15 +0530

    i jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,8 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+
 
 const userSchema = new Schema(
     {
@@ -30,6 +34,17 @@ const userSchema = new Schema(
         }
     },{ timestamps: true }
 )
+
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password);
+}
 
 
 export const User = mongoose.model("User", userSchema);
