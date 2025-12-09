1d0eb7d4c38acc81ea8db5d06170b16e530aabad
Sanju Roy <sanjuroy806@gmail.com>
2025-12-10 01:43:39 +0530

    add user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,5 +46,31 @@ userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password, this.password);
 }
 
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+    },process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES
+    }
+  )
+}
+userSchema.methods.gennerateRefreshToken = function (){
+    jwt.sign({
+        _id: this._id, 
+    },process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES
+    }
+)
+}
+
+
+
+
+
+
+
 
 export const User = mongoose.model("User", userSchema);
