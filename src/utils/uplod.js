a55885c1921f2fcb888f3c605fe0b0364bb50b8e
Sanju Roy <sanjuroy806@gmail.com>
2025-12-11 18:12:16 +0530

    multer setup

---
+++ src/utils/uplod.js
@@ -0,0 +1,27 @@
+import { v2 } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+
+const uploadToCloudinary = (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        //upload the file on cloudinary
+        const response = cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        })
+        console.log("response", response.url);
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath);// remove the file from local uploads folder
+        return null;
+    }
+
+}
+
+export { uploadToCloudinary };
