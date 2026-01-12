fe787179c579731068ef0044c640bc48b8ad811d
Sanju Roy <sanjuroy806@gmail.com>
2026-01-13 00:19:51 +0530

    Refactor import statements and add initial functionality for playlist, subscription, and tweet controllers

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,5 @@
 import {asyncHandler} from "../utils/asyncHendler.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import ApiResponse from "../utils/ApiResponse.js"
 import {ApiError} from "../utils/ApiError.js"
 import mongoose, { isValidObjectId } from "mongoose"
 import {Video} from "../models/video.model.js"
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,12 +1,12 @@
-import { asyncHendler } from "../utils/asyncHendler.js";
+import { asyncHandler } from "../utils/asyncHendler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import { Like } from "../models/like.model.js";
 import { Video } from "../models/video.model.js"
 import { isValidObjectId } from "mongoose";
 
 
-const videoLike = asyncHendler(async (req, res) => {
+const videoLike = asyncHandler(async (req, res) => {
 
     const { VideoId } = req.params // video id validation
 
@@ -42,6 +42,22 @@ const videoLike = asyncHendler(async (req, res) => {
             .status(200)
             .json(new ApiResponse(200, { isLiked: true }, "liked add successfully"))
     }
+})
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    //TODO: toggle like on comment
+
+})
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params
+    //TODO: toggle like on tweet
+}
+)
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
 })
 
 export { videoLike }
